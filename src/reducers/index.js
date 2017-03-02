48adc52352089de6ef1190ad47219ee12a468079
Yofret Rios <yofret.rios@jobsity.com>
2017-03-02 11:56:23 -0500

    Wire are selection reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,13 @@
 
 	import { combineReducers } from 'redux';
 	import LibraryReducer from './LibraryReducer';
+	import SelectionReducer from './SelectionReducer'
 
 //-------------------------------
 // Export Reducers 
 //-------------------------------
 	
 	export default combineReducers({
-		libraries: LibraryReducer
+		libraries: LibraryReducer,
+		selectedLibraryId: SelectionReducer
 	});
